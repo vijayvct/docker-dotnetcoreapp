e95410e23ba9129eb94f57234d74bbedef3994ea
Vijay Vishwakarma <VijayV@citiustech.com>
2020-12-22 22:51:28 +0530

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace docker_dotnetcoreapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
